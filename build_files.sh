97f67dae04f6ddbc287640215cafca92c2f70346
shaharia-islam-png <islamshaharia2006@gmail.com>
2026-09-11 12:56:40 +0600

    fix: updated vercel config and break system packages flag

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD START"
-python3.9 -m pip install --break-system-packages -r requirements.txt
-python3.9 LumenDev/manage.py collectstatic --noinput --clear
+python3 -m pip install -r requirements.txt --break-system-packages
+python3 LumenDev/manage.py collectstatic --noinput --clear
 echo "BUILD END"
